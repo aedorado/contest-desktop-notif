bd9fac540186f1e906c1b2e5f83994453354469a
Anurag El Dorado <annurag94@gmail.com>
2016-02-22 19:02:41 +0530

    Started.

---
+++ check.py
@@ -0,0 +1,11 @@
+from all_imports import *
+
+url = URL('https://contesttrackerapi.herokuapp.com/')
+response = url.fetch()
+
+jso = json.loads(response.read())
+ongoing = jso['result']['ongoing']
+upcoming = jso['result']['upcoming']
+
+print ongoing
+print upcoming
